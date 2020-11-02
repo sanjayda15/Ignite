ee36638da5901cea01f3c349cacc5193e24c310d
Sanjay Das <sanjayda15@gmail.com>
2020-11-02 12:57:17 +0530

    changes

--- static/resources/js/main.js
+++ static/resources/js/main.js
@@ -13,7 +13,11 @@ $(document).ready(function () {
   );
 });
 
-
+if ($(window).width() < 769) {
+          $('#navlast').addClass('order-last');
+      } else {
+          $('#navlast').removeClass('order-last');
+      }
 
 $(document).ready(function () {
   $(".box").hover(function () {
